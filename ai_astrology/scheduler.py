c3dc4f53377d4bb571f5c6f343b93068fe07dee9
William Pettengill <Dewey@Sidney-Hook.local>
2018-11-25 23:20:45 -0500

    bug fix

---
+++ ai_astrology/scheduler.py
@@ -0,0 +1,14 @@
+#scheduler
+
+import astrology_data_pipeline as adp 
+import astrology as ast 
+
+if __name__ == '__main__':
+	#df, udf, sheet, data, w, ww = adp.transform()
+	if adp.ping():
+		adp.main()
+		ast.main()	
+	else:
+		break
+	
+	
