93d95d5e39c80f559ba351062fbe2c15fa2adec4
William Pettengill <Dewey@Sidney-Hook.local>
2018-11-27 01:20:26 -0500

    bug fix

--- ai_astrology/scheduler.py
+++ ai_astrology/scheduler.py
@@ -6,8 +6,10 @@ import astrology as ast
 if __name__ == "__main__":
 	#df, udf, sheet, data, w, ww = adp.transform()
 	if adp.ping():
+		print('success')
 		adp.main()
 		ast.main()	
 	else:
 		pass
 
+* * * * * source activate py36 && bash /home/willpettengill.github.io/ai_astrology/main.bash && source deactivate
