637ca4bafff79a748ea1346c213bd5ae1d88d365
wpettengill <wpettengill@tripadvisor.com>
2022-01-18 19:25:18 -0500

    transacts

--- datadog/main.bash
+++ datadog/main.bash
@@ -8,10 +8,14 @@ python etl_1.py --sheetid 1f0_gCDLQYOTC9sr9B6y94PpljRWlZ97wvboccTpnScA --range b
 
 python etl_1.py --sheetid 1f0_gCDLQYOTC9sr9B6y94PpljRWlZ97wvboccTpnScA --range pepsi-mic-drop --push true --csv data/os_token_pepsi-mic-drop.csv --header true
 
-python icytools.py 
+python icytools.py --lookback 1,7,30 --endpoint stats --start 2022-01-17 --backfill 48
+
+python icytools.py --endpoint transactions
 
 # icy
 python etl_1.py --sheetid 1f0_gCDLQYOTC9sr9B6y94PpljRWlZ97wvboccTpnScA --range icy_transactions --push true --append false --header true --csv data/icy_transactions.csv
+
+python etl_1.py --sheetid 1f0_gCDLQYOTC9sr9B6y94PpljRWlZ97wvboccTpnScA --range icy_stats --push true --append false --header true --csv data/icy_stats.csv
 
 # stats
 python etl_1.py --sheetid 1f0_gCDLQYOTC9sr9B6y94PpljRWlZ97wvboccTpnScA --range os_stats_endpoint --push true --append true --csv data/os_stats_budverse-cans-heritage-edition.csv --header false
