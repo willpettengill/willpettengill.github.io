7a7de440430b3b63419d704961db976d515a7a54
wpettengill <wpettengill@tripadvisor.com>
2022-01-21 15:05:39 -0500

    FOR SALE status

--- datadog/osea_data_1.py
+++ datadog/osea_data_1.py
@@ -3,6 +3,8 @@ import requests
 import pandas as pd
 import argparse
 import datetime as dt
+import time 
+
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.max_colwidth', 20)
 
@@ -13,6 +15,13 @@ def fieldsFromAPI(c):
 	num_sales=c.get('num_sales', {})
 	owner = c.get('owner', {}).get('address', {})
 	collection_slug = c.get('collection', {}).get('slug', {})
+	if c.get('sell_orders'):
+		if c.get('sell_orders')[0].get('expiration_time',1)>time.time():
+			for_sale=True
+		else:
+			for_sale=False
+	else:
+		for_sale=False
 	try:
 		last_sale_usd= c.get('last_sale').get('payment_token').get('usd_price') if (c.get('last_sale') or None) is not None else None
 		last_sale_eth= c.get('last_sale').get('payment_token').get('eth_price') if (c.get('last_sale') or None) is not None else None
@@ -31,20 +40,18 @@ def fieldsFromAPI(c):
 	last_sale_ds = c.get('created_date', {})
 	contract_address = c.get('asset_contract', {}).get('address', {})
 	datapull_ds = dt.date.today().strftime('%Y-%m-%d')
-	asset_data = {'token_id': token_id, 'owner':owner, 'collection': collection_slug, 'contract_address':contract_address, 'datapull_ds': datapull_ds,  'permalink': permalink, 'last_sale_usd': last_sale_usd, 'last_sale_eth': last_sale_eth, 'top_bid': top_bid, 'num_sales': num_sales, 'last_sale_from_address': last_sale_from_address, 'last_sale_from_user': last_sale_from_user, 'last_sale_to_address': last_sale_to_address, 'last_sale_to_user': last_sale_to_user, 'traits': traits, 'last_sale_ds': last_sale_ds}
+	asset_data = {'token_id': token_id, 'owner':owner, 'collection': collection_slug, 'contract_address':contract_address, 'datapull_ds': datapull_ds,  'permalink': permalink, 'last_sale_usd': last_sale_usd, 'last_sale_eth': last_sale_eth, 'top_bid': top_bid, 'num_sales': num_sales, 'last_sale_from_address': last_sale_from_address, 'last_sale_from_user': last_sale_from_user, 'last_sale_to_address': last_sale_to_address, 'last_sale_to_user': last_sale_to_user, 'traits': traits, 'last_sale_ds': last_sale_ds, 'for_sale':for_sale}
 	return asset_data
 
-def dfFromCollection(collection, test):
+def dfFromCollection(collection, headers, test):
 	asset_list = []	
 	offset=0
 	pagination_flag=True
 	while pagination_flag==True:
-		print(offset)
 		url = "https://api.opensea.io/api/v1/assets?order_direction=desc&offset={1}&collection={0}&limit=50".format(collection, offset)
-		response = requests.request("GET", url)
+		response = requests.request("GET", url, headers=headers)
 		print(response.text)
 		assets = dict(response.json()).get('assets', {})
-		print(len(assets))
 		for c in assets:
 			asset_data = fieldsFromAPI(c)
 			asset_list.append(asset_data)
@@ -57,7 +64,6 @@ def dfFromCollection(collection, test):
 			break
 	df = pd.DataFrame.from_records(asset_list, columns=asset_list[0].keys()).sort_values(by='token_id', ascending=True) # ['token_id', 'permalink', 'last_sale_usd', 'last_sale_eth', 'top_bid', 'num_sales']
 	df = df.fillna(value=0)
-	print(df)
 	return df, assets
 
 def StatsByCollection(collection, addToday=True, addCollection=True):
@@ -69,8 +75,17 @@ def StatsByCollection(collection, addToday=True, addCollection=True):
 	if addCollection:
 		stats['collection'] = collection
 	df = pd.DataFrame.from_records([stats])
-	print(df)
 	return df
+
+def ListingStatus(collection, api_key):
+	url = "https://api.opensea.io/api/v1/events?collection_slug=budverse-cans-heritage-edition&event_type=successful&only_opensea=false&offset=0&limit=100"
+	headers = {
+	    "Accept": "application/json",
+	    "X-API-KEY": api_key
+	}
+	response = requests.request("GET", url, headers=headers)
+	
+	return dict(response.json()).get("asset_events",{})
 
 if __name__ == '__main__':
 	
@@ -82,11 +97,16 @@ if __name__ == '__main__':
 	parser.add_argument("--endpoint", help='e.g. stats or token')
 	args = parser.parse_args()
 	collections = args.collections.split(',')
-	
+	api_key = '610c839f631b446f98c8ed1f2611d89e'
+	headers = {
+    "Accept": "application/json",
+    "X-API-KEY": '610c839f631b446f98c8ed1f2611d89e'
+	}
+
 	if args.endpoint == 'token':
 		for collection in collections:
 			print(collection)
-			df, assets = dfFromCollection(collection, args.test)
+			df, assets = dfFromCollection(collection, headers, args.test)
 			file_to_write = (args.outfolder or 'data') + '/' + (args.outfile_prefix or '') + collection + '.csv'
 			df.to_csv(file_to_write, index=False, header=list(df.columns))
 
