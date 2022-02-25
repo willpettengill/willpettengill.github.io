394ae5526b1e2ead5f17a8b8bd51718918bd4dff
wpettengill <wpettengill@tripadvisor.com>
2022-02-25 11:36:59 -0500

    seed and debug changes

--- datadog/write_db.py
+++ datadog/write_db.py
@@ -180,7 +180,7 @@ if __name__ == '__main__':
     
     if args.debug:
         runTests()
-        cursor.execute('select user()';)
+        cursor.execute('select user();')
         cursor.fetchall()
         cursor.execute('select * from vnft.token_metadata limit 3') # test is like token_metadata
         res_ = []
