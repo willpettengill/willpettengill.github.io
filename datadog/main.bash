edd8d386a70957014693278e6990eb77a55eb300
wpettengill <wpettengill@tripadvisor.com>
2022-02-26 16:55:29 -0500

    db script update

--- datadog/main.bash
+++ datadog/main.bash
@@ -1,17 +1,17 @@
 # transactions: ICY
 python icytools.py --endpoint transactions
 
-python etl_1.py --sheetid 1f0_gCDLQYOTC9sr9B6y94PpljRWlZ97wvboccTpnScA --range icy_transactions --push true --append false --header true --csv data/icy_transactions.csv
+#python etl_1.py --sheetid 1f0_gCDLQYOTC9sr9B6y94PpljRWlZ97wvboccTpnScA --range icy_transactions --push true --append false --header true --csv data/icy_transactions.csv
 
 # tokens: Opensea 
 python osea_data_1.py --endpoint token
 
-python etl_1.py --sheetid 1f0_gCDLQYOTC9sr9B6y94PpljRWlZ97wvboccTpnScA --range token_metadata --push true --csv data/token_metadata.csv --header true
+#python etl_1.py --sheetid 1f0_gCDLQYOTC9sr9B6y94PpljRWlZ97wvboccTpnScA --range token_metadata --push true --csv data/token_metadata.csv --header true
 
 # stats: ICY
 python icytools.py --lookback 1,7 --endpoint stats --start daily --backfill 5
 
-python etl_1.py --sheetid 1f0_gCDLQYOTC9sr9B6y94PpljRWlZ97wvboccTpnScA --range icy_stats --push true --append false --header true --csv data/icy_stats.csv
+#python etl_1.py --sheetid 1f0_gCDLQYOTC9sr9B6y94PpljRWlZ97wvboccTpnScA --range icy_stats --push true --append false --header true --csv data/icy_stats.csv
 
 # SQL
 python write_db.py
